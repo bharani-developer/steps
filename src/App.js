62f76e443530044767bd206d25c4cca88ed4370f
Bharani karthikeyan <bharani.developer@gmail.com>
2024-06-12 16:34:38 +0530

    updating new code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn HTML",
+  "Learn CSS",
+  "Learn Bootstrap",
+  "Learn JavaScript",
+  "Learn JQuery",
+  "Learn React.js",
+  "Learn Node.js",
+  "Learn Express.js",
+  "Learn MongoDB",
+  "MERN Completed"
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+  function handleNext() {
+    if (step < 10) setStep(step + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          {/* numbers */}
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          {/* message */}
+          <p className="message">
+            step {step} : {messages[step - 1]}
+          </p>
+          {/* buttons */}
+          <div className="buttons">
+            <button onClick={handlePrevious}>Previous</button>
+            <button onClick={handleNext}>Next</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
